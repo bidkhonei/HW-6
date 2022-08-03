cc155392632895d4dfd169322b44a7ff301ceb67
bidkhonei <hassan317zahedi@gmail.com>
2022-08-03 19:45:05 +0330

    Run

---
+++ src/main/java/Runer.java
@@ -0,0 +1,17 @@
+
+import service.AppObject;
+
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+
+public class Runer {
+    public static void main(String[] args) throws Exception {
+
+        AppObject appObject = new AppObject();
+
+
+        appObject.app();
+
+}}
